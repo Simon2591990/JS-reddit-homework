e4b8178bbdb6109a193011ac500a3337b00051f9
Simon Charters <simon259@hotmail.co.uk>
2021-01-15 20:14:12 +0000

    filtered some unwanted display features

--- src/App.vue
+++ src/App.vue
@@ -40,6 +40,9 @@ export default {
 </script>
 
 <style>
+body{
+  background-color: black;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,8 +1,12 @@
 <template lang="html">
   <div>
-      <div v-for="post in postList">
+      <div id="post"  v-for="post in postList" v-if="!post.data.is_video">
         <h2 v-on:click="selectPost(post)">{{post.data.title}}</h2>
-        <img v-if="selectedPost === post" :src="post.data.thumbnail">
+        <img v-on:click="unSelectPost(post)" id="image" v-if="selectedPost === post && !post.data.is_self" :src="post.data.url">
+        <!-- <img v-on:click="selectPost(post)"id="thumbnail"  :src="post.data.thumbnail"> -->
+        <img v-on:click="selectPost(post)"id="thumbnail" v-if="!post.data.is_self && post !== selectedPost" :src="post.data.thumbnail">
+        <h3 v-if="post.data.is_self">Text Post</h3>
+        <p v-on:click="unSelectPost(post)" v-if="post.data.is_self && post === selectedPost" >{{post.data.selftext}}</p>
         </div>
   </div>
 </template>
@@ -19,20 +23,36 @@ export default {
     },
     data(){
         return{
-            selectedPost: null
+            selectedPost: null,
         }
+    
         
     },
     methods:{
         selectPost: function(post){
             this.selectedPost = post
-
-        }    
+        },
+        unSelectPost: function(post){
+            this.selectedPost = null
+        },  
+        // fetch post.data.permalink to display comments
     }
 }
 
 </script>
 
 <style>
+#thumbnail{
+    width: 50px
+}
+#image{
+    width: 500px
+}
+#post{
+    background-color: rgb(16, 16, 16);
+}
+#black{
+    background-color: rgb(0, 0, 0);
+}
 
 </style>
